8018f9e765bdf56ae656c98df238b649ef24d57a
Lexi Santoro <lexisantoro1@gmail.com>
2019-01-04 16:50:02 -0500

    deploy

--- server/db/index.js
+++ server/db/index.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
-var mongoUri = 'mongodb://localhost:27017/bookings'; 
-
+//var mongoUri = 'mongodb://localhost:27017/bookings'; 
+var mongoUri = 'mongodb://alexandra1:alexandra1@ds251622.mlab.com:51622/lexibook';
 // Connect Mongoose to our local MongoDB via URI specified above and export it below
 var db = mongoose.connect(mongoUri, { useNewUrlParser: true } );
 
