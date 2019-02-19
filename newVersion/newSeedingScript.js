52c72614d134cb499801ad85079563a40eb1e3e3
justinposer <jposer1@gmail.com>
2019-02-19 15:52:34 -0500

    updated record amount

--- newVersion/newSeedingScript.js
+++ newVersion/newSeedingScript.js
@@ -94,7 +94,7 @@ var insertAllData = (total, part) => {
 
 // first number is total records to insert and the second number is the batchsize
 
-insertAllData(1000000, 10000);
+insertAllData(10000000, 10000);
 
 // the command below will insert all created records into the database
 
